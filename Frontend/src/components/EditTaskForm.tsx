3962d951d3f134b65795507373bfe41a0043b5db
GIHAA <gihansudeeparandimal@gmail.com>
2024-01-22 21:57:15 +0530

    wip

--- Frontend/src/components/EditTaskForm.tsx
+++ Frontend/src/components/EditTaskForm.tsx
@@ -207,7 +207,7 @@ const EditUserForm = ({ setDisplayUpdateForm, fetchData, target }: any) => {
                   className="inline-flex items-center px-5 py-2.5  text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-primary-900 hover:bg-blue-800"
                   onClick={onUpdateUser}
                 >
-                  Update User
+                  Update Task
                 </button>
 
                 <button
