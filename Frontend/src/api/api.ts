48412cb3f99c9aefbe46aca37f8ec67df94fa134
GIHAA <gihansudeeparandimal@gmail.com>
2024-01-23 09:06:25 +0530

    Feat: fe changes added

--- Frontend/src/api/api.ts
+++ Frontend/src/api/api.ts
@@ -1,2 +1,2 @@
-export const BE_URL = "http://localhost/api/v1";
-//export const BE_URL = "https://task-manager-app.azurewebsites.net/api/v1";
+//export const BE_URL = "http://localhost/api/v1";
+export const BE_URL = "https://task-manager-app.azurewebsites.net/api/v1";
